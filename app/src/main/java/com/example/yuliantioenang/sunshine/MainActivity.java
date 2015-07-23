0a27e60dc0a4b4dffcbca251f9ec0efed7e7c86c
Yulianti Oenang <yulianti.oenang@gdplabs.id>
2015-07-23 13:34:19 +0700

    tablet ui

--- app/src/main/java/com/example/yuliantioenang/sunshine/MainActivity.java
+++ app/src/main/java/com/example/yuliantioenang/sunshine/MainActivity.java
@@ -1,20 +1,36 @@
 package com.example.yuliantioenang.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback{
+    String mLocation;
+    boolean mTwoPane;
+    private static final String DETAILFRAGMENT_TAG = "DFTAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mLocation = Utility.getPreferredLocation(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(findViewById(R.id.weather_detail_container) != null) {
+            mTwoPane = true;
+            if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.weather_detail_container, new DetailActivityFragment(), DETAILFRAGMENT_TAG)
+                    .commit();
+            }
+        } else {
+            mTwoPane = false;
+        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,8 +50,61 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
+        } else if (id == R.id.action_map) {
+            openPreferedLocation();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openPreferedLocation(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = preferences.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", location).build();
+        mapIntent.setData(geoLocation);
+        if(mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String currentLocation = Utility.getPreferredLocation(this);
+        if (currentLocation != null && ! currentLocation.equals(mLocation)){
+            ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_forecast);
+            if(ff != null) {
+                ff.onLocationChanged();
+            }
+
+            DetailActivityFragment df = (DetailActivityFragment)getSupportFragmentManager().findFragmentByTag(DETAILFRAGMENT_TAG);
+            if ( null != df ) {
+                df.onLocationChanged(currentLocation);
+            }
+
+            mLocation = currentLocation;
+        }
+    }
+
+    @Override
+    public void onItemSelected(Uri dateUri) {
+        if (mTwoPane) {
+            Bundle arg = new Bundle();
+            arg.putParcelable(DetailActivityFragment.URI, dateUri);
+
+            DetailActivityFragment df = new DetailActivityFragment();
+            df.setArguments(arg);
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.weather_detail_container, df, DETAILFRAGMENT_TAG)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivityFragment.class);
+            intent.setData(dateUri);
+            startActivity(intent);
+        }
     }
 }
